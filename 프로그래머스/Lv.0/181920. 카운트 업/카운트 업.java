4b317aad7fa30b74e0464e2814f98afbc066377a
boy who loves potato <120919984+potatobboi@users.noreply.github.com>
2024-01-15 20:32:58 +0900

    [level Lv.0] Title: 카운트 업, Time: 0.03 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int start_num, int end_num) {
+        List<Integer> intList = new ArrayList<>();
+        for (int i = start_num; i <= end_num; i++) {
+            intList.add(i);
+        }
+        return intList;
+    }
+}
