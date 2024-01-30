98658467a2722ce0f74f3b13b5ee5ed5f8b5bd2a
boy who loves potato <120919984+potatobboi@users.noreply.github.com>
2024-01-31 05:46:16 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,12 +1,10 @@
 class Solution {
     public long solution(long n) {
-        long answer = 0;
+        long answer = -1;
         double sqrt = Math.sqrt(n);
         
         if (sqrt % 1 == 0) {
-            answer = (long) Math.pow(sqrt + 1, 2);
-        } else {
-            answer = -1;
+            answer = (long)Math.pow((sqrt + 1), 2);
         }
         return answer;
     }
