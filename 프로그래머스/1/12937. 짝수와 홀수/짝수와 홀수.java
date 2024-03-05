73be31064e6defff9969561f54f1a947bb07ccf5
boy who loves potato <120919984+potatobboi@users.noreply.github.com>
2024-03-05 09:09:01 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,9 +1,11 @@
 class Solution {
     public String solution(int num) {
+        String answer = "";
         if (num % 2 == 0) {
-            return "Even";
+            answer = "Even";
         } else {
-            return "Odd";
+            answer = "Odd";
         }
+        return answer;
     }
 }
