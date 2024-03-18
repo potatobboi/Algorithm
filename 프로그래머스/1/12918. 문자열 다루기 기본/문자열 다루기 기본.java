7149a54f624953a934b95cbed4597729c54e5eec
boy who loves potato <120919984+potatobboi@users.noreply.github.com>
2024-03-18 09:28:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean solution(String s) {
-        if (s.length() == 4 || s.length() == 6 ) {
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) < '0' || s.charAt(i) > '9') {
-                    return false;
-                }
-            } return true;
+        if (s.length() == 4 || s.length() == 6) {
+            try {
+                int x = Integer.parseInt(s);
+                return true;
+            } catch(Exception e) {
+                return false;
+            }
         } else return false;
     }
 }
