a38060fe4be3660d65669fbaf700b6d254ccb9e1
boy who loves potato <120919984+potatobboi@users.noreply.github.com>
2024-03-22 09:27:24 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.19 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,15 +1,17 @@
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "";
+        StringBuilder str = new StringBuilder();
         int count = 0;
         
         for (int i = 0; i < seoul.length; i++) {
             if (!seoul[i].equals("Kim")) {
                 count++;
             } else {
-                answer = "김서방은 " + count + "에 있다";
+                str.append("김서방은 ");
+                str.append(count);
+                str.append("에 있다");
             }
         }
-        return answer;
+        return str.toString();
     }
 }
