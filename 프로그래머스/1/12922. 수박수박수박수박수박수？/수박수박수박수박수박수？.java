c9b7594b372774540c245a666cbea2791d152d51
boy who loves potato <120919984+potatobboi@users.noreply.github.com>
2024-04-19 22:36:12 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.40 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,12 @@
 class Solution {
-    public String solution(int n) {
-        StringBuilder str = new StringBuilder();
-        
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 != 0) {
-                str.append("수");
-            } else {
-                str.append("박");
-            }
-        }
-        return str.toString();
+  public String solution(int n) {
+    String answer = "";
+ 
+    for (int i = 1; i <= n; i++) {
+      if (i % 2 != 0) answer += "수";
+      else answer += "박";
     }
+ 
+    return answer;
+  }
 }
